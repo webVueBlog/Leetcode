dc1db1771db1fec6013d9e705844a2db26707f04
GDone <2979986590@qq.com>
2021-12-14 18:05:01 +0800

    fix: new_file.html

---
+++ 1.两数之和.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=javascript
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+
+};
+// @lc code=end
+
