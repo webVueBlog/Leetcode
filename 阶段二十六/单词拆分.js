a2a7b03c625e712386e990c6daebacce2f9592de
webVueBlog <2979986590@qq.com>
2022-04-20 22:14:00 +0800

    update: --

--- 阶段二十六/单词拆分.js
+++ 阶段二十六/单词拆分.js
@@ -5,5 +5,16 @@
  139. 单词拆分
  */
 var wordBreak = function(s, wordDict) {
-
+    const n = s.length;
+    const dp = new Array(n + 1).fill(false);
+    dp[0] = true;
+    for (let i = 1; i <= n; i++) {
+        for (let j = 0; j < i; j++) {
+            if (dp[j] && wordDict.indexOf(s.slice(j, i)) !== -1) {
+                dp[i] = true;
+                break;
+            }
+        }
+    }
+    return dp[n];
 };
